f6c4c1d3b0c443c14681d2337da68f6dd344c3a0
Alessio Narder <alessio.narder@gmail.com>
2022-01-27 19:36:40 +0100

    test cron job

--- pyfunc/handler.py
+++ pyfunc/handler.py
@@ -1,6 +1,9 @@
+from . import helper
+
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    return "Hi from pyfunc"
+    return helper.fib(5)
